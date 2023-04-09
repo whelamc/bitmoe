b203a7b40708ea914b24aac22ed2c2d3204874b8
whelam <whelamc@gmail.com>
2023-04-09 16:06:55 +0800

    test all

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,11 @@
 'use client';
  
-import {useTranslations} from 'next-intl';
-
 import { products } from "@/lib/products";
 import Banner from "@/components/Banner";
 import ProductCard from "@/components/ProductCard";
 
 export default function Page() {
-  const t = useTranslations('Index');
+
   return (
     <>
       <Banner />
