66f39a486e226f4603aaf0b6086e0d8b11fb80d5
whelam <whelamc@gmail.com>
2023-04-09 17:52:37 +0800

    fixed

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,17 +1,19 @@
-'use client';
-import Image from 'next/image';
-import Link from '@/components/Link'; 
+"use client";
+import Image from "next/image";
+import Link from "@/components/Link";
 
-import {useTranslations} from 'next-intl';
+import { useTranslations } from "next-intl";
 
 type information = {
   footerCopyright: string;
-  footerCopyrightSlug: string;
+  footerSecurity: string;
+  footerSecurityUrl?: string;
   footerICP: string;
+  footerICPUrl?: string;
 };
 
 export default function Footer({ information }: { information: information }) {
-  const t = useTranslations('Footer');
+  const t = useTranslations("Footer");
   return (
     <div className="w-full text-center">
       <div className="bg-endeavour-600 dark:bg-endeavour-800 dark:text-gray-500">
@@ -26,44 +28,45 @@ export default function Footer({ information }: { information: information }) {
                 height={26}
               />
             </div>
-            <div className="py-6 text-left text-sm">
-            {t('description')}
-            </div>
+            <div className="py-6 text-left text-sm">{t("description")}</div>
           </div>
           <div className="flex flex-1 items-center justify-end text-left leading-none sm:flex-none sm:w-full sm:justify-normal sm:flex-row sm:px-2">
             <div className="ml-10 sm:ml-0 sm:flex-1">
-              <p className="mb-4 text-lg">{t('aboutTitle')}</p>
+              <p className="mb-4 text-lg">{t("aboutTitle")}</p>
               <a href={`mailto:service@bitmoe.com`}>
-                <p className="mb-4 hover:underline">{t('subsContact')}</p>
+                <p className="mb-4 hover:underline">{t("subsContact")}</p>
               </a>
               <a href={`https://shop155149662.taobao.com/`}>
-                <p className="mb-4 hover:underline">{t('subsTaobao')}</p>
+                <p className="mb-4 hover:underline">{t("subsTaobao")}</p>
               </a>
               <a href={`mailto:service@bitmoe.com`}>
-                <p className="mb-4 hover:underline">{t('subsJoin')}</p>
+                <p className="mb-4 hover:underline">{t("subsJoin")}</p>
               </a>
             </div>
             <div className="ml-10 sm:ml-0 sm:flex-1">
-              <p className="mb-4 text-lg">{t('infoTitle')}</p>
+              <p className="mb-4 text-lg">{t("infoTitle")}</p>
               <Link href={`/posts/termsofservice`}>
-                <p className="mb-4 hover:underline">{t('subsAgreement')}</p>
+                <p className="mb-4 hover:underline">{t("subsAgreement")}</p>
               </Link>
               <Link href={`/posts/privacypolicy`}>
-                <p className="mb-4 hover:underline">{t('subsPrivate')}</p>
+                <p className="mb-4 hover:underline">{t("subsPrivate")}</p>
               </Link>
               <Link href={`/posts/intellectualproperty`}>
-                <p className="mb-4 hover:underline">{t('subsKnowledge')}</p>
+                <p className="mb-4 hover:underline">{t("subsKnowledge")}</p>
               </Link>
             </div>
           </div>
         </div>
       </div>
       <div className="bg-endeavour-700 dark:bg-endeavour-800 dark:text-gray-500">
-        {' '}
+        {" "}
         <div className="m-auto max-w-[900px] py-4 md:max-w-[720px] md:min-h-min sm:w-full sm:px-2">
           <p>{information.footerCopyright}</p>
-          <p>{information.footerCopyrightSlug}</p>
-          <p>{information.footerICP}</p>
+          <div>
+            {" "}
+            <a href={information.footerSecurityUrl}>{information.footerSecurity}</a>｜
+            <a href={information.footerICPUrl}>{information.footerICP}</a>
+          </div>
         </div>
       </div>
     </div>
--- client/lib/introduction.ts
+++ client/lib/introduction.ts
@@ -1,12 +1,15 @@
 type Item = {
   footerCopyright: string;
-  footerCopyrightSlug: string;
+  footerSecurity: string;
+  footerSecurityUrl?: string;
   footerICP: string;
+  footerICPUrl?: string;
 };
 
 export const information: Item = {
-  footerCopyright: `© 2016-2023, Shenzhen Bitmoe Infomation Technology Co.,Ltd. All
-    Rights Reserved.`,
-  footerCopyrightSlug: `深圳比特萌信息技术有限公司版权所有`,
-  footerICP: `浙ICP备18031538号`,
+  footerCopyright: `Copyright © 2015-2023 Shenzhen BitMOE Information Technology Co., Ltd. All rights reserved.`,
+  footerSecurity: `粤公网安备44030502000499号`,
+  footerSecurityUrl: `https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030502000499`,
+  footerICP: `粤ICP备16092592号`,
+  footerICPUrl: `https://beian.miit.gov.cn`
 };
